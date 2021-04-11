8cf98a904dc11ecd0608fd61dec9ce215467bb9d
elye <elye.project@gmail.com>
2021-04-11 20:26:00 +1000

    Update to new version on jetpack compose

--- app/src/main/java/com/example/recompose/MainActivity.kt
+++ app/src/main/java/com/example/recompose/MainActivity.kt
@@ -15,7 +15,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.setContent
+import androidx.activity.compose.setContent
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 
